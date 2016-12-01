df231858275b2f9b212a81928e6faeee0ce1dcdb
Nikolay <niknikpopov@gmail.com>
2016-12-01 14:27:34 +0200

    Add files via upload

--- js/directives/directives.js
+++ js/directives/directives.js
@@ -1,1 +1,12 @@
+var appDirectives = angular.module('myapp');
 
+//Data File Getting Directive from Input Field
+appDirectives.directive('getFile', function(){
+    return {
+        restrict: 'A',
+        link: function(scope, element, attrs) {
+            var onChangeFunc = scope.$eval(attrs.getFile);
+            element.bind('change', onChangeFunc)
+        }
+    }
+});
