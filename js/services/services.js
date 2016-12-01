dbb3539e5bd6019c9698c463af11cdd27a78fe05
Nikolay <niknikpopov@gmail.com>
2016-12-01 14:28:24 +0200

    Add files via upload

--- js/services/services.js
+++ js/services/services.js
@@ -1,1 +1,29 @@
+var appServices = angular.module('myapp');
 
+//Data Exchange Service
+appServices.service('storage', function() {
+
+    var status = {
+        userID: '',
+        userFirstName: '',
+        userLastName: '',
+        isLoggedIn: true,
+        isAdmin: true,
+        isForgotPass: true,
+        isProfileEditOn: true,
+        isMainMenuOn: true,
+        isAdminMenuOn: true,
+    };
+
+    this.getDefault = function() {
+        return status
+    };
+
+    this.getStatus = function(name) {
+        return status[name]
+    };
+
+    this.setStatus = function(name, value) {
+        status[name] = value
+    }
+});
