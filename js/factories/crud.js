5ac7dc5d8a3a692606e41a3a313d35773d58b6f4
Nikolay <niknikpopov@gmail.com>
2016-12-01 14:29:20 +0200

    Add files via upload

--- js/factories/crud.js
+++ js/factories/crud.js
@@ -1,1 +1,81 @@
+var app = angular.module('myapp');
 
+function response(response) {
+     if(response.$$state.status === 0) return response; // success
+};
+
+app.factory('Giphy', function($http) {
+
+	var host = 'http://api/giphy.com';
+	var key = 'dc6zaTOxFJmzC';
+
+	return {
+		get: function(query) {
+			return response($http({
+				method: 'GET',
+				url: host + '/v1/gifs/search',
+				params: {
+					api_key: key,
+					q: query
+				},
+				headers: {
+					'Content-Type': 'application/json',
+					'Access-Control-Allow-Origin': '*',
+					'Access-Control-Allow-Methods': "'GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'",
+					'Access-Control-Allow-Headers': 'Content-Type, Accept, x-requested-with, cache-control',
+					'Access-Control-Allow-Credentials': true
+				}
+			}))
+		},
+		trending: function(data) {
+			return response($http({
+				method: 'GET',
+				url: host + '/v1/gifs/trending',
+				params: {
+					api_key: key,
+					data: data
+				}
+			}))
+		},
+		translate: function(data) {
+			return response($http({
+				method: 'GET',
+				url: host + '/v1/gifs/translate',
+				params: {
+					api_key: key,
+					data: data
+				}
+			}))
+		},
+		random: function(data) {
+			return response($http({
+				method: 'GET',
+				url: host + '/v1/gifs/random',
+				params: {
+					api_key: key,
+					data: data
+				}
+			}))
+		},
+		getGifById: function(id) {
+			return response($http({
+				method: 'GET',
+				url: host + '/v1/gifs/' + id,
+			}))
+		},
+		getGifByIds: function(list) {
+			return response($http({
+				method: 'GET',
+				url: host + '/v1/gifs/',
+				params: list
+			}))
+		},
+		upload: function(data) {
+			return response($http({
+				method: 'POST',
+				url: host + '/v1/gifs/',
+				params: data
+			}))
+		},
+	}
+});
