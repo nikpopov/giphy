fd43be86b4c05357fe82e258f5c4b2ae8a6438cc
Nikolay <niknikpopov@gmail.com>
2016-12-01 14:25:37 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,1 +1,55 @@
+var app = angular.module('myapp', ['ngRoute']);
+
+//Page Routing Configuring
+app.config(['$routeProvider', '$locationProvider', '$httpProvider',
+    function($routeProvider, $locationProvider, $httpProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: '../view/main.html',
+                controller: 'mainController'
+            })   
+            .when('/gallery', {
+                templateUrl: '../view/galery.html',
+                controller: 'galleryController'
+            })            
+            .when('/search', {
+                templateUrl: '../view/galery.html',
+                controller: 'searchController'
+            })
+            .when('/collection', {
+                templateUrl: '../view/galery.html',
+                controller: 'collectionController'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });  
+}]);
+
+
+app.service('storage', function() {
+    var userStatus = {
+        userID: '',
+        userFirstName: '',
+        userLastName: '',
+        isLoggedIn: true,
+        isAdmin: true,
+        isForgotPass: true
+    };
+    this.getDefault = function() {
+        return userStatus
+    };
+    this.getStatus = function(name) {
+        return userStatus[name]
+    };
+    this.setStatus = function(name, value) {
+        userStatus[name] = value
+    }
+});
+
+
+app.run(['$window', '$rootScope', 'storage', function($window, $rootScope, storage) {
+    
+    storage.setStatus('isAdmin', false);
+
+}]);
 
